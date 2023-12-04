e07042c4eac57c543ac2435200b4a417f5582179
Yves-Alexis LORVELLEC <yvesalexis.lorvellec2023@campus-eni.fr>
2023-12-04 16:19:24 +0100

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My App-fil rouge"
+include(":app")
+ 
